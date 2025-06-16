8ffeab29499238579248319db72b9a83cb516a8f
ryanohmm <oboyleryan1@gmail.com>
2025-06-16 15:44:31 -0400

    Update for Quote Card Express 3.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,24 @@
 "use strict"
 
+async function getRandomImage() {
+    const client_id = "";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json()
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
+
+/*
+
 const elements = {
     quote: document.getElementById("quote"),
     author: document.getElementById("author"),
@@ -35,4 +54,4 @@ function loopThroughQuotes() {
     }, 3000);
 }
 
-setTimeout(loopThroughQuotes, 3000);
+setTimeout(loopThroughQuotes, 3000); */
